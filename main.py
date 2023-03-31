38cb143284debeca7ad2eac0880d6b02dd96fa34
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-31 13:10:52 +0300

    ++ created conecction with redis

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from redis_om import get_redis_connection
 
 app = FastAPI()
+
+redis = get_redis_connection(
+    host='localhost',
+    port=6379,
+    password="password",
+    decode_responses=True,
+    db=0,
+)
 
 
 @app.get("/")

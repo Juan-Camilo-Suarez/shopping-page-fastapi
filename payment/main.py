bad37ca92104025aed2eb15e267a63609445fa40
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-04-01 19:13:03 +0300

    ++ main into payment created model order and view create order

--- payment/main.py
+++ payment/main.py
@@ -1,3 +1,5 @@
+from starlette.requests import Request
+import requests as requests
 from fastapi import FastAPI
 from redis_om import get_redis_connection, HashModel
 from starlette.middleware.cors import CORSMiddleware
@@ -20,3 +22,32 @@ redis = get_redis_connection(
 )
 
 
+class Order(HashModel):
+    product_id: str
+    price: float
+    fees: float
+    total: float
+    quantity: int
+    status: str  # pending, completed and refundend
+
+    class Meta:
+        database = redis
+
+
+@app.post("/orders")
+async def create(request: Request):
+    body = await request.json()
+
+    req = requests.get('http://127.0.0.1:8000/products/%s' % body['id'])
+    product = req.json()
+
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=body['quantity'],
+        status='pending'
+    )
+    order.save()
+    return order
